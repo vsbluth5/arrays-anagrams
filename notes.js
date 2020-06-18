edfe91992fb741331aec91748be717a5a03b055d
Glitch (upperlinecode-anagrams-array-methods-javascript-8) <none>
2020-06-18 13:53:14 +0000

    👞😅 Checkpoint ./notes.js:3856783/678

--- notes.js
+++ notes.js
@@ -1,1 +1,4 @@
+// When we are ready to do the practice, change the <script> tag in the index.html
+// so that it linkss to array.js instead of notes.js
+
 console.log(`This is where I will practice arrays`)
